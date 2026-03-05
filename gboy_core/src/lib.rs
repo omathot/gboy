f09b7a758e771c93f16d926fd58b32a70d529aa2
omathot <omathot@proton.me>
2026-03-05 19:43:30 +0100

    added flag register

--- gboy_core/src/lib.rs
+++ gboy_core/src/lib.rs
@@ -10,31 +10,54 @@ const MAX_SPRITES: usize = 128;
 const NUM_KEYS: usize = 10;
 const NUM_SOUND_CHANNELS: usize = 6;
 
-pub type Byte = u8;
-pub type SignedByte = i8;
+const ZERO_FLAG_BYTE_POS: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POS: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POS: u8 = 5;
+const CARRY_FLAG_BYTE_POS: u8 = 4;
 
-// const PROGRAM_START:
-
+#[derive(Debug, Clone, Copy)]
 pub struct FlagsRegister {
 	zero: bool,
 	subtract: bool,
 	half_carry: bool,
 	carry: bool,
 }
+impl std::convert::From<FlagsRegister> for u8 {
+	fn from(flag: FlagsRegister) -> Self {
+		(if flag.zero { 1 } else { 0 }) << ZERO_FLAG_BYTE_POS
+			| (if flag.subtract { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POS
+			| (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POS
+			| (if flag.carry { 1 } else { 0 }) << CARRY_FLAG_BYTE_POS
+	}
+}
+impl std::convert::From<u8> for FlagsRegister {
+	fn from(byte: u8) -> Self {
+		let zero = ((byte >> ZERO_FLAG_BYTE_POS) & 0b1) != 0;
+		let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POS) & 0b1) != 0;
+		let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POS) & 0b1) != 0;
+		let carry = ((byte >> CARRY_FLAG_BYTE_POS) & 0b1) != 0;
 
+		FlagsRegister {
+			zero,
+			subtract,
+			half_carry,
+			carry,
+		}
+	}
+}
 pub struct Registers {
 	a: u8,
 	b: u8,
 	c: u8,
 	d: u8,
 	e: u8,
-	f: u8,
+	f: FlagsRegister,
 	h: u8,
 	l: u8,
 }
 impl Registers {
 	pub fn get_af(&self) -> u16 {
-		(self.a as u16) << 8 | self.f as u16
+		(self.a as u16) << 8 | u8::from(self.f) as u16
 	}
 	pub fn get_bc(&self) -> u16 {
 		(self.b as u16) << 8 | self.c as u16
@@ -44,7 +67,7 @@ impl Registers {
 	}
 	pub fn set_af(&mut self, value: u16) {
 		self.a = ((value & 0xFF00) >> 8) as u8;
-		self.f = (value & 0xFF) as u8;
+		self.f = FlagsRegister::from((value & 0xFF) as u8);
 	}
 	pub fn set_bc(&mut self, value: u16) {
 		self.b = ((value & 0xFF00) >> 8) as u8;
@@ -56,10 +79,23 @@ impl Registers {
 	}
 }
 
+enum Instruction {
+	ADD(ArithmeticTarget),
+}
+enum ArithmeticTarget {
+	A,
+	B,
+	C,
+	D,
+	E,
+	H,
+	L,
+}
+
 pub struct GBoy {
 	pc: u16,
 	screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
-	keys: [Byte; NUM_KEYS],
+	keys: [u8; NUM_KEYS],
 }
 impl GBoy {
 	pub fn new() -> Self {
